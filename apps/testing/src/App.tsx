711c0fdd97d226c4961c15c7c1a371a4d55e11da
Adrian Karp <adrian.karp@ibm.com>
2022-10-02 10:40:26 +0300

    backtracking on UI library bug

--- apps/testing/src/App.tsx
+++ apps/testing/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
-import { Button } from '@playground/components-web'
+
 import '@fontsource/roboto/400.css'
 
 function App() {
   return (
     <div className="App">
-      <Button onClick={() => {}} label={'label'} />
+      <button onClick={() => {}}>button</button>
     </div>
   )
 }
--- packages/components-web/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@playground/components-web'
