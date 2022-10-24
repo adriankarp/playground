b4f9062013d6cb149046cfa9865c2046dc75cdd3
Adrian Karp <adrian.karp@ibm.com>
2022-10-24 06:16:57 +0300

    testing linting

--- packages/components/web/src/Button/Button.tsx
+++ packages/components/web/src/Button/Button.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { default as BaseButton } from '@mui/material/Button';
 
-export const Button = ({ onClick, label }: Props) => {
+export function Button({ onClick, label }: Props) {
   return <BaseButton onClick={onClick}>{label}</BaseButton>;
-};
+}
 
 type Props = {
   onClick: () => void;
   label: string;
 };
-console.log('asda');
-console.log('asda');
 
 export default Button;
