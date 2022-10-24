2d9dd3c159068c4f770679e21b281cb667adb997
Adrian Karp <karp.adrian@protonmail.com>
2022-10-23 20:24:37 -0700

    Setup/prepare web components (#19)
    * modify documentation
    
    * setup eslint prettier and husky pre-commit
    
    * remove lint-staged
    
    * testing linting
    
    * activate lint runner
    
    * disable function-component-definition and arrow-body-style rules
    
    * modify pull request template
    
    Co-authored-by: Adrian Karp <adrian.karp@ibm.com>

--- packages/components/web/src/Button/Button.tsx
+++ packages/components/web/src/Button/Button.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { default as BaseButton } from '@mui/material/Button'
+import React from 'react';
+import { default as BaseButton } from '@mui/material/Button';
 
 export const Button = ({ onClick, label }: Props) => {
-  return <BaseButton onClick={onClick}>{label}</BaseButton>
-}
+  return <BaseButton onClick={onClick}>{label}</BaseButton>;
+};
 
 type Props = {
-  onClick: () => void
-  label: string
-}
+  onClick: () => void;
+  label: string;
+};
 
-export default Button
+export default Button;
