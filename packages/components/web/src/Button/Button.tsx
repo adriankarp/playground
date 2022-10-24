eff8089aa0604fe13fe570d613eb132dee8b8e21
Adrian Karp <adrian.karp@ibm.com>
2022-10-24 06:22:39 +0300

    disable function-component-definition and arrow-body-style rules

--- packages/components/web/src/Button/Button.tsx
+++ packages/components/web/src/Button/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { default as BaseButton } from '@mui/material/Button';
 
-export function Button({ onClick, label }: Props) {
+export const Button = ({ onClick, label }: Props) => {
   return <BaseButton onClick={onClick}>{label}</BaseButton>;
-}
+};
 
 type Props = {
   onClick: () => void;
