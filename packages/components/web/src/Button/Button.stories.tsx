65caa1926b3d55bcc3f092f700304a61a339edfa
Adrian Karp <adrian.karp@ibm.com>
2022-10-25 15:45:43 +0300

    deleted old config

--- packages/components/web/src/Button/Button.stories.tsx
+++ packages/components/web/src/Button/Button.stories.tsx
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import { ComponentMeta } from '@storybook/react';
-// import { Button } from './Button';
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from './Button';
 
-// export default {
-//   title: 'Button',
-//   component: Button
-// } as ComponentMeta<typeof Button>;
+export default {
+  title: 'Button',
+  component: Button
+} as ComponentMeta<typeof Button>;
 
-// // ----------------------------------------------------------------
+// ----------------------------------------------------------------
 
-// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-// // ----------------------------------------------------------------
+// ----------------------------------------------------------------
 
-// export const Primary = Template.bind({});
-// Primary.args = { label: 'Primary button', onClick: () => {} };
+export const Primary = Template.bind({});
+Primary.args = { label: 'Primary button', onClick: () => {} };
