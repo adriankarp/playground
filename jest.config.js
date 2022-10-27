7f463be84e96aaeed82cb8afe3f31825f91aa264
Adrian Karp <adrian.karp@ibm.com>
2022-10-27 13:17:35 +0300

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePaths: ['<rootDir>'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['/dist/']
 };
