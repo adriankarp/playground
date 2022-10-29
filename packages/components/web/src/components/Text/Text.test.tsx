5609901d7ca47129822c92738a2161d3a588a880
Adrian Karp <adrian.karp@ibm.com>
2022-10-29 10:49:26 +0300

    added text component

--- packages/components/web/src/components/Text/Text.test.tsx
+++ packages/components/web/src/components/Text/Text.test.tsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import Text from './Text';
 
 it('matches snapshot', () => {
-  const component = renderer.create(<Text label="label" />);
+  const component = renderer.create(<Text variant="h1">Example text</Text>);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
