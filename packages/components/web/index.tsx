a4cb11721d031111a3d9eea1b25e613988a4904f
Adrian Karp <adrian.karp@ibm.com>
2022-10-29 14:24:32 +0300

    exported components

--- packages/components/web/index.tsx
+++ packages/components/web/index.tsx
@@ -1,2 +1,4 @@
 export * from './src/components/Text/Text';
 export * from './src/components/Button/Button';
+export * from './src/components/IconButton/IconButton';
+export * from './src/components/LoadingButton/LoadingButton';
