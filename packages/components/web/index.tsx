f2e2eb999d384e14bbed81be4ab7c56f5859e28f
Adrian Karp <adrian.karp@ibm.com>
2022-10-29 15:54:17 +0300

    added ToggleButton

--- packages/components/web/index.tsx
+++ packages/components/web/index.tsx
@@ -2,3 +2,5 @@ export * from './src/components/Text/Text';
 export * from './src/components/Button/Button';
 export * from './src/components/IconButton/IconButton';
 export * from './src/components/LoadingButton/LoadingButton';
+export * from './src/components/ButtonGroup/ButtonGroup';
+export * from './src/components/ToggleButton/ToggleButton';
