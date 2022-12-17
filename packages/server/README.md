ceaa714bc00eec9ccf13c10da10a80bfecb71386
Adrian Karp <Adrian.Karp@ibm.com>
2022-12-17 15:17:27 +0200

    code cleanup

---
+++ packages/server/README.md
@@ -0,0 +1,3 @@
+# Server
+
+Add README here
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,23 +1,10 @@
 import config from './config/index';
 import app from './app';
-import graphqlServer from './graphql';
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import pkg from 'body-parser';
-
-const { json } = pkg;
 
 const start = async () => {
   try {
-    await graphqlServer.start();
-    await app.use(
-      '/graphql',
-      cors<cors.CorsRequest>(),
-      json(),
-      expressMiddleware(graphqlServer)
-    );
     await app.listen(config.PORT);
-    console.log(`🚀  GraphQL server running at port: ${config.PORT}`);
+    console.log(`🚀 Server running at port: ${config.PORT}`);
   } catch {
     console.log('Unable to start GraphQL server');
   }
